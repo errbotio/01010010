cc061259931a3a0063220d858ed3572bb301911a
Nick Groenen <nick@groenen.me>
2016-03-05 22:35:53 +0100

    Validate processing running state after restart

--- scripts/upgrade_bot.sh
+++ scripts/upgrade_bot.sh
@@ -5,3 +5,8 @@ set -x
 
 (cd repository && pip install --upgrade .)
 sudo supervisorctl restart errbot-gitter
+
+# supervisorctl doesn't exit with a non-zero code when process fails to start.
+# See https://github.com/Supervisor/supervisor/issues/24
+sudo supervisorctl status errbot-gitter | grep --quiet RUNNING
+exit $?
